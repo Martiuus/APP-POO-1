2f1c29f47ac40d99626f062c4e0101de10bdcfcd
Martiuus <mzf.ycb.11@gmail>
2025-02-20 20:21:16 -0500

    Aplicando Polimorfismo a la Clase Alumno y Docente

--- src/model/Docente.java
+++ src/model/Docente.java
@@ -2,4 +2,15 @@ package model;
 
 public class Docente extends Persona{
 
+    @Override
+    public boolean registrarAsistencia(
+            String codIdentificador,
+            String fechaAsistencia,
+            String horaAsistencia,
+            Boolean valorAsistencia
+    ){
+        System.out.println("Aplicar logica" + "para la asistencia del " + "docente");
+        return true;
+    }
+
 }
